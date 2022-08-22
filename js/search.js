17bf3eb6296f562ec73244cc1078c8921cfee1b2
Mark <83771053+Mark-Liston@users.noreply.github.com>
2022-08-22 11:00:39 +0800

    Modularised search proof of concept
    Moved API calls into their own methods and generally cleaned up the code.

--- js/search.js
+++ js/search.js
@@ -2,11 +2,15 @@ import fetch from "node-fetch";
 import rls from "readline-sync";
 
 let found = false;
-while (!found)
-{
-    let searchParam = rls.question("Enter a code to search: ");
 
-    // Searches for the top 20 search results corresponding to the input.
+/** 
+ * Searches handbook for items matching given search parameter and year.
+ * @param {String} searchParam String normally in search box that is searched for.
+ * @param {Date} year Year that item must be available.
+ * @param {Function} callback Function called after results are received from search.
+ */ 
+async function searchHandbook(searchParam, year, size, callback)
+{
     await fetch("https://handbook.murdoch.edu.au/api/es/search",
     {
         // Request boilerplate.
@@ -22,9 +26,162 @@ while (!found)
             "Referrer-Policy": "strict-origin-when-cross-origin"
         },
         // Actual search query.
-        "body": "{\"query\":{\"bool\":{\"must\":[{\"term\":{\"live\":true}},{\"multi_match\":{\"query\":\"" + searchParam + "\",\"type\":\"phrase_prefix\",\"max_expansions\":20,\"fields\":[\"*.code^10\",\"*.title^8\",\"contenttype\"]}},[{\"bool\":{\"minimum_should_match\":\"50%\",\"should\":[{\"query_string\":{\"fields\":[\"*availableInYears*\"],\"query\":\"*2022*\"}}]}},{\"bool\":{\"minimum_should_match\":\"50%\",\"should\":[{\"query_string\":{\"fields\":[\"*status*\"],\"query\":\"Active\"}}]}}]],\"filter\":[{\"terms\":{\"contenttype\":[\"murdoch_psubject\",\"murdoch_pcourse\",\"murdoch_paos\"]}}]}},\"sort\":[\"_score\"],\"from\":0,\"size\":100,\"track_scores\":true,\"_source\":{\"includes\":[\"*.code\",\"*.name\",\"*.award_titles\",\"*.keywords\",\"urlmap\",\"contenttype\"],\"excludes\":[\"\",null]}}",
+        // body does not contain an actual JSON object, just a JSON string. Here, it is written as a JSON
+        // object then stringified to improve readability.
+        "body": JSON.stringify(
+        {
+            "query":
+            {
+                "bool":
+                {
+                    "must":
+                    [
+                        {
+                            "term":
+                            {
+                                "live":true
+                            }
+                        },
+                        {
+                            "multi_match":
+                            {
+                                // Search box input.
+                                "query": searchParam,
+                                "type":"phrase_prefix",
+                                "max_expansions":20,
+                                "fields":
+                                [
+                                    "*.code^10",
+                                    "*.title^8",
+                                    "contenttype"
+                                ]
+                            }
+                        },
+                        [
+                            {
+                                "bool":
+                                {
+                                    "minimum_should_match":"50%",
+                                    "should":
+                                    [
+                                        {
+                                            "query_string":
+                                            {
+                                                "fields":
+                                                [
+                                                    "*availableInYears*"
+                                                ],
+                                                // Year item is available.
+                                                "query":"*" + year + "*"
+                                            }
+                                        }
+                                    ]
+                                }
+                            },
+                            {
+                                "bool":
+                                {
+                                    "minimum_should_match":"50%",
+                                    "should":
+                                    [
+                                        {
+                                            "query_string":
+                                            {
+                                                "fields":
+                                                [
+                                                    "*status*"
+                                                ],
+                                                // Whether item is active.
+                                                "query":"Active"
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        ]
+                    ],
+                    "filter":
+                    [
+                        {
+                            "terms":
+                            {
+                                "contenttype":
+                                [
+                                    "murdoch_psubject",
+                                    "murdoch_pcourse",
+                                    "murdoch_paos"
+                                ]
+                            }
+                        }
+                    ]
+                }
+            },
+            "sort":
+            [
+                "_score"
+            ],
+            "from":0,
+            // Number of results to return.
+            "size": size,
+            "track_scores":true,
+            "_source":
+            {
+                "includes":
+                [
+                    "*.code",
+                    "*.name",
+                    "*.award_titles",
+                    "*.keywords",
+                    "urlmap",
+                    "contenttype"
+                ],
+                "excludes":
+                [
+                    "",
+                    null
+                ]
+            }
+        }),
+
         "method": "POST"
-    }).then(response => response.json()).then(function(data) // Extracts JSON from response and passes that data to callback.
+    // Extracts JSON from response then passes that data to callback.
+    }).then(response => response.json()).then(data => callback(data));
+}
+
+/**
+ * Fetches handbook entry for item with given version and code.
+ * @param {Number} version Version number.
+ * @param {String} code Item's code e.g., ICT283, MJ-CMSC, MN-NETS.
+ * @param {Function} callback Function called after item is fetched.
+ */
+async function fetchItem(version, code, callback)
+{
+    fetch("https://handbook.murdoch.edu.au/api/content/render/false/query/+contentType:murdoch_psubject%20+murdoch_psubject.version:" + version + "%20+murdoch_psubject.code:" + code + "%20+deleted:false%20+working:true%20+live:true%20+languageId:1%20/orderBy/modDate%20desc",
+    {
+        // Request boilerplate.
+        "headers":
+        {
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "en-US,en;q=0.9",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "Referer": "https://handbook.murdoch.edu.au/results/" + version + "/" + code,
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": null,
+        "method": "GET"
+    // Extracts JSON from response then passes that data to callback.
+    }).then(response => response.json()).then(data => callback(data));
+}
+
+while (!found)
+{
+    let searchParam = rls.question("Enter a code to search: ");
+    let year = new Date().getFullYear();
+
+    // Searches for 20 results in the handbook matching search parameter and year.
+    await searchHandbook(searchParam, year, 20, function(data)
     {
         let results = data.contentlets;
         let allResults = "";
@@ -51,21 +208,8 @@ while (!found)
             // If result is a unit, rather than a major, minor, etc.
             if (results[i].contentTypeLabel == "Unit")
             {
-                fetch("https://handbook.murdoch.edu.au/api/content/render/false/query/+contentType:murdoch_psubject%20+murdoch_psubject.version:" + version + "%20+murdoch_psubject.code:" + code + "%20+conHost:9e1aa987-69e8-4624-91c2-b31291b29099%20+deleted:false%20+working:true%20+live:true%20+languageId:1%20/orderBy/modDate%20desc",
-                {
-                    "headers":
-                    {
-                        "accept": "application/json, text/plain, */*",
-                        "accept-language": "en-US,en;q=0.9",
-                        "sec-fetch-dest": "empty",
-                        "sec-fetch-mode": "cors",
-                        "sec-fetch-site": "same-origin",
-                        "Referer": "https://handbook.murdoch.edu.au/results/" + version + "/" + code,
-                        "Referrer-Policy": "strict-origin-when-cross-origin"
-                    },
-                    "body": null,
-                    "method": "GET"
-                }).then(matchResponse => matchResponse.json()).then(function(matchData)
+                // Gets handbook entry for unit.
+                fetchItem(version, code, function(matchData)
                 {
                     // Contains data such as assessments, requisites, learning outcomes, etc.
                     let extraData = JSON.parse(matchData.contentlets[0].data);
